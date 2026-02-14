4b549541a67640446f5840de37e4376b5f851e2c
Ali Abubakar Mohammed <alimuhammedabubakar6334@gmail.com>
2026-02-14 13:31:57 +0100

    refactor: remove student management module and related files

--- backend/students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('students/', views.student_list),
-    path('students/create/', views.student_create),
-    path('students/delete/<int:id>/', views.student_delete),
-]
